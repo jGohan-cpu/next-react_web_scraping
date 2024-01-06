1f2aa539b74034830cfb42df4517c1b70d0377cb
jGohan-cpu <jjoseriveram01@gmail.com>
2024-01-06 01:35:56 -0400

    debugging

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,61 +10,72 @@ export const maxDuration = 10;
 export const dynamic = 'force-dynamic'
 export const revalidate = 0;
 
-export async function GET() {
+export async function GET(request: Request) {
     try {
         connectToDB();
 
         const products = await Product.find({});
 
-        if (!products) throw new Error("No products found");
+        if (!products) throw new Error("No product fetched");
 
+        // ======================== 1 SCRAPE LATEST PRODUCT DETAILS & UPDATE DB
         const updatedProducts = await Promise.all(
             products.map(async (currentProduct) => {
+                // Scrape product
                 const scrapedProduct = await scrapeSheinProduct(currentProduct.url);
 
-                if (!scrapedProduct) throw new Error("No product found");
-                const updatedPriceHistory: any = [
+                if (!scrapedProduct) return;
+
+                const updatedPriceHistory = [
                     ...currentProduct.priceHistory,
-                    { price: scrapedProduct.originalPrice }
-                ]
+                    {
+                        price: scrapedProduct.currentPrice,
+                    },
+                ];
 
                 const product = {
                     ...scrapedProduct,
                     priceHistory: updatedPriceHistory,
                     lowestPrice: getLowestPrice(updatedPriceHistory),
                     highestPrice: getHighestPrice(updatedPriceHistory),
-                }
+                };
 
+                // Update Products in DB
                 const updatedProduct = await Product.findOneAndUpdate(
-                    { url: scrapedProduct.url },
-                    product,
+                    {
+                        url: product.url,
+                    },
+                    product
                 );
 
-                //Check each product status and send email accordingly
+                // ======================== 2 CHECK EACH PRODUCT'S STATUS & SEND EMAIL ACCORDINGLY
+                const emailNotifType = getEmailNotifType(
+                    scrapedProduct,
+                    currentProduct
+                );
 
-                const emailNotifType = getEmailNotifType(scrapedProduct, currentProduct);
-
-                if (emailNotifType && updatedProduct.users.lenght > 0) {
+                if (emailNotifType && updatedProduct.users.length > 0) {
                     const productInfo = {
                         title: updatedProduct.title,
-                        url: updatedProduct.url
-                    }
-
+                        url: updatedProduct.url,
+                    };
+                    // Construct emailContent
                     const emailContent = await generateEmailBody(productInfo, emailNotifType);
-
-                    const userEmails = updatedProduct.users.map((user: any) => user.email)
-
+                    // Get array of user emails
+                    const userEmails = updatedProduct.users.map((user: any) => user.email);
+                    // Send email notification
                     await sendEmail(emailContent, userEmails);
                 }
 
                 return updatedProduct;
             })
-        )
+        );
 
         return NextResponse.json({
-            message: 'Ok', data: updatedProducts
-        })
-    } catch (error) {
-        console.log(error)
+            message: "Ok",
+            data: updatedProducts,
+        });
+    } catch (error: any) {
+        throw new Error(`Failed to get all products: ${error.message}`);
     }
 }
